ff658062d3269c824fc7a0d11e4e6a81a3d92e78
jesusaguilera <jesusaguilera09@gmail.com>
2021-03-06 07:54:57 +0100

    getting user from array objects of users

--- src/functional/arrays.js
+++ src/functional/arrays.js
@@ -1,16 +1,20 @@
 const numbers = [1, 2, 3];
 
-// Adding
+// Adding at the end
 let addedLast =  [...numbers, 4];
+console.log("Added at last", addedLast);
+
+// Adding in the beginning
 let addedInit =  [4, ...numbers];
+console.log("Added at first", addedInit);
+
+// Adding in X position
 let index = numbers.indexOf(2);
 let addedInPositionTwo =  [
   ...numbers.slice(0, index), 
   4, 
   ...numbers.slice(index)
 ];
-console.log("Added at last", addedLast);
-console.log("Added at init", addedInit);
 console.log("Added at second position", addedInPositionTwo);
 
 
@@ -22,3 +26,13 @@ console.log("Removed 2", removedTwo);
 // Update
 let updateTwo = numbers.map(n => n === 2 ? 50 : n);
 console.log("Update 2", updateTwo);
+
+// Get element from array objects through its id
+const users = [
+  {id: 1, name: "Martin", surname: "McFly"},
+  {id: 2, name: "Helen", surname: "Reagan"},
+  {id: 3, name: "Susan", surname: "Sogan"},
+];
+
+const user = users.findIndex(u => u.id === 1);
+console.log(users[user]);
